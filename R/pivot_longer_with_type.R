2c7bb2087d1e42117134beda2a6adfd045ec10b2
christiaanpauw <christiaan.pauw@nova.org.za>
2024-08-15 08:56:07 +0200

    Documentation

--- R/pivot_longer_with_type.R
+++ R/pivot_longer_with_type.R
@@ -1,22 +1,22 @@
 #' pivot_longer_with_type
-#' @param data
-#' @param cols
-#' @param ...
+#'
+#' @param data data.frame
+#' @param ... Passed to pivot_longer
 #' @param names_to Character. What you want to call the attribute column. Default "name"
 #' @param tripplenames Logical. Default TRUE. If TRUE, name columns subject, predicate, object and type
 #'
 #' @return tibble with four columns:"subject", "predicate", "object", "type"
 #' @export
 #' @importFrom magrittr %<>%
-#' @imprt dplyr
-#' @example
+#' @import dplyr
+#' @examples
 #' df1 = pivot_longer_with_type(iris)
 #' df1 %>% select(predicate, type) %>% distinct()
 
 pivot_longer_with_type <- function(data, names_to = "name", tripplenames = TRUE, ...) {
 
 # Get original data types
-types <- sapply(data, typeof)
+types <- sapply(data, class)
 dftypes <- tibble(name = names(types), type = types)
 names(dftypes)[1] <- names_to
 
@@ -45,4 +45,31 @@ res %>% dplyr::mutate(type = case_when(
   type ==  "list" ~ "array",
   TRUE ~ "string"))
 
+}
+
+pivot_wider_by_type <- function(res, subject = "subject", predicate = "predicate", object = "object") {
+
+  dftypes <- res %>% select(predicate, type) %>%
+    distinct() %>%
+    dplyr::mutate(type = case_when(
+    type == "string" ~ "character",
+    type == "double" ~ "numeric",
+    type == "boolean" ~ "logical",
+    type == "date" ~ "Date",
+    type == "date" ~ "POSIXct",
+    type == "integer" ~ "integer",
+    type ==  "array" ~ "list",
+    TRUE ~ "string"))
+
+  res %>% distinct() %>% select(-type) %>%
+    pivot_wider(id_cols = all_of(subject), names_from = predicate, values_from = object) %>%
+    mutate(across(all_of(dftypes$predicate),
+                  ~ map2(.x, dftypes$type[dftypes$predicate == cur_column()], function(col, type) {
+                    switch(type,
+                           "character" = as.character(col),
+                           "integer" = as.integer(col),
+                           "numeric" = as.numeric(col),
+                           "double" = as.double(col),
+                           col) # Return as is if type doesn't match any cases
+                  }) %>% unlist()))
 }
