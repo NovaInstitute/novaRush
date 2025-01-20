a0f1e2cf8eda46c457d5b45531ecc8802cd42d8b
Jij Human <28621883@sun.ac.za>
2025-01-20 08:07:01 +0200

    Add QueryInstance

--- R/QueryInstance.R
+++ R/QueryInstance.R
@@ -1,34 +1,121 @@
-library(R6)
-library(flureeCrypto)
 
-QueryInstance <- R6Class(
-  "QueryInstance",
+#' Class providing objects with methods to query a Fluree instance.
+#' 
+#' @docType class
+#' @importFrom R6 R6Class
+#' @importFrom jsonlite toJSON fromJSON
+#' @importFrom flureeCrypto serialize_jws public_key_from_private account_id_from_public
+#' @importFrom httr POST
+#' 
+#' @export
+QueryInstance <- R6Class("QueryInstance",
   public = list(
+    #' @field query (`list()`)\cr
+    #' The list representation of a query to be sent to the Fluree instance.
     query = NULL,
-    config = NULL,
-    signedQuery = '',
     
-    # constructor method
+    #' @field config (`list()`)\cr
+    #' Configuration parameters of the instance.
+    config = NULL,
+    
+    #' @field signedTransaction (`string`)\cr
+    #' The JWT of the query.
+    signedQuery = '',
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #' 
+    #' @param transaction (`list()`)\cr
+    #'   The transaction to be sent to the Fluree instance.
     initialize = function(query, config) {
       
       self$query <- query
       self$config <- config
-      
-      # merge contexts if applicable
+
       defaultContext <- config$defaultContext %||% list()
-      queryContext <- query[["@context"]] %||% list()
+      transactionContext <- transaction[["@context"]] %||% list()
       
-      if (!is.null(defaultContext) || !is.null(queryContext)) {
-        self$query[['@context']] <- merge_contexts(defaultContext, transactionContext)
+      if (!is.null(defaultContext) || !is.null(transactionContext)) {
+        self$transaction[['@context']] <- mergeContexts(defaultContext, transactionContext)
       }
       
       if (config$signMessages) {
         self$sign()
       }
     },
-    
+  
+    #' @description
+    #' This method sends the configured query to the host.
+    #' The Fluree instance must be 'connected' before querying or transacting.
+    #' If `signMessages = TRUE` the JWT will be sent to the host.
     send = function() {
+      if (nzchar(self$signedQuery)) {
+        contentType <- 'application/jwt'
+      } else {
+        contentType <- 'application/json'
+      }
       
-    }
+      params <- generateFetchParams(self$config, 'query', contentType)
+      url <- params$url
+      fetchOptions <- params$config
+      
+      if (!is.null(self$signedQuery)) {
+        params$body <- self$signedQuery
+      } else {
+        params$body <- toJSON(self$query, auto_unbox = TRUE, pretty = TRUE)
+      }
+      
+      response <- POST(
+        url = url,
+        add_headers(`Content-Type` = params$config$headers$`Content-Type`),
+        body = params$body,
+        encode = "raw"
+      )
+      
+      print(content(response, as = "text"))
+    },
     
-  ))
+    #' @description
+    #' This method is used to sign the configured query.
+    #' This method is called automatically when `signMessages = TRUE` and requires
+    #' a privateKey to be either passed as a parameter or configured within the 
+    #' `config`.
+    #' 
+    #' @param privateKey (`string`)\cr
+    #'   The private key to use for message signing (represented as a hex string).
+    #' @return [QueryInstance]
+    sign = function(privateKey = NULL) {
+      if (!is.null(privateKey)) {
+        key <- privateKey
+      } else {
+        key <- self$config$privateKey
+      }
+      
+      if (is.null(key)) {
+        stop("privateKey must be provided in either the transaction or the config")
+      }
+      
+      input <- toJSON(self$query, auto_unbox = TRUE, pretty = FALSE)
+      signedQuery <- flureeCrypto:::serialize_jws(as.character(input), key)
+      
+      self$signedQuery <- signedQuery
+      return(self)
+    },
+    
+    #' @description
+    #' Returns the signed query (if it has been set).
+    #' 
+    #' @return (`string`).
+    getSignedQuery = function() {
+      return(self$signedQuery)
+    },
+    
+    #' @description
+    #' Returns the query body.
+    #' 
+    #' @return (`string`).
+    getQuery = function() {
+      return(self$query)
+    }
+  )
+)
